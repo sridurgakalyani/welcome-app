5fb95bcf2cba826bc642d7fe996deb5c35da82fe
sridurgakalyani <gsreedurgakalyani@gmail.com>
2022-03-13 12:20:04 +0000

    first commit

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,28 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  onSubscribed = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const {buttonText} = this.getButtonText()
+    return (
+      <div className="app-container">
+        <h1 className="heading">Welcome</h1>
+        <p className="description">Thank you! Happy Learning</p>
+        <button onClick={this.onSubscribed}>{buttonText}</button>
+      </div>
+    )
+  }
+}
+export default Welcome
